3f598fad02564a45520f64f0014ff6ad53cc7f49
andbin <andbin@users.noreply.github.com>
2021-02-04 17:32:05 +0100

    Add Kelvin temperature unit

---
+++ src/main/java/it/andbin/temperatureconverter/model/KelvinTemperatureUnit.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Andrea Binello ("andbin")
+ *
+ * This file is part of the "Quarkus RESTful Temperature Converter" project
+ * and is released under the MIT License. See one of the license files
+ * included in the root of the project for the full text of the license.
+ */
+
+package it.andbin.temperatureconverter.model;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class KelvinTemperatureUnit extends TemperatureUnit {
+    public KelvinTemperatureUnit() {
+        super("kelvin", "Kelvin", "K", 273.15, 373.15, "William Thomson, 1st Baron Kelvin");
+    }
+
+    @Override
+    public double toKelvin(double x) {
+        return x;
+    }
+
+    @Override
+    public double fromKelvin(double k) {
+        return k;
+    }
+}
