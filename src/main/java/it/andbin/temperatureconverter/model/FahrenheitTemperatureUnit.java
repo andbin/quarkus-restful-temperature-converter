c5263b9fef8b2ef8a4367fad8949ea5e49a8f529
andbin <andbin@users.noreply.github.com>
2021-02-04 17:50:14 +0100

    Improve Fahrenheit conversion

--- src/main/java/it/andbin/temperatureconverter/model/FahrenheitTemperatureUnit.java
+++ src/main/java/it/andbin/temperatureconverter/model/FahrenheitTemperatureUnit.java
@@ -18,11 +18,11 @@ public class FahrenheitTemperatureUnit extends TemperatureUnit {
 
     @Override
     public double toKelvin(double x) {
-        return (x + 459.67) * 5 / 9;
+        return (x + 459.67) / 1.8;
     }
 
     @Override
     public double fromKelvin(double k) {
-        return k * 9 / 5 - 459.67;
+        return k * 1.8 - 459.67;
     }
 }
