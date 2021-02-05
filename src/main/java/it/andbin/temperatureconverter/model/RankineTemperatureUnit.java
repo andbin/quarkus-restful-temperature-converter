a7998b03eb6749eb1a176e1adb791c4232b986ab
andbin <andbin@users.noreply.github.com>
2021-02-05 09:38:49 +0100

    Add Rankine temperature unit

---
+++ src/main/java/it/andbin/temperatureconverter/model/RankineTemperatureUnit.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Andrea Binello ("andbin")
+ *
+ * This file is part of the "Quarkus RESTful Temperature Converter" project
+ * and is released under the MIT License. See one of the license files
+ * included in the root of the project for the full text of the license.
+ */
+
+package it.andbin.temperatureconverter.model;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class RankineTemperatureUnit extends TemperatureUnit {
+    public RankineTemperatureUnit() {
+        super("rankine", "Rankine", "°R, °Ra", 491.67, 671.67, "Macquorn Rankine");
+    }
+
+    @Override
+    public double toKelvin(double x) {
+        return x / 1.8;
+    }
+
+    @Override
+    public double fromKelvin(double k) {
+        return k * 1.8;
+    }
+}
